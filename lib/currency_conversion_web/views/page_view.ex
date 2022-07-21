135c9319fd48c61bffb5bcd90a0e65b9b83aff8c
Rabea Gleissner <rabeagleissner@gmail.com>
2022-07-21 09:24:56 +0300

    Format converted amount for display

--- lib/currency_conversion_web/views/page_view.ex
+++ lib/currency_conversion_web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule CurrencyConversionWeb.PageView do
   use CurrencyConversionWeb, :view
+
+  def format_amount(amount) do
+    case is_float(amount) do
+      true -> :erlang.float_to_binary(amount, [decimals: 2])
+      false -> ""
+    end
+  end
 end
--- test/currency_conversion_web/views/page_view_test.exs
+++ test/currency_conversion_web/views/page_view_test.exs
@@ -1,3 +1,12 @@
 defmodule CurrencyConversionWeb.PageViewTest do
   use CurrencyConversionWeb.ConnCase, async: true
+  alias CurrencyConversionWeb.PageView
+
+  test "Formats amount with two decimals" do
+    assert PageView.format_amount(98.237989) == "98.24"
+  end
+
+  test "Returns empty string if the amount is not a float" do
+    assert PageView.format_amount("") == ""
+  end
 end

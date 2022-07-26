d35162fda5620a8b5737a3327684d7b197425119
Rabea Gleissner <rabeagleissner@gmail.com>
2022-07-26 08:38:34 +0300

    Retrieve rate from cache to avoid further API calls

--- lib/currency_conversion/api_cache.ex
+++ lib/currency_conversion/api_cache.ex
@@ -5,18 +5,32 @@ defmodule CurrencyConversion.ApiCache do
 
   def start_link(_), do: GenServer.start_link(__MODULE__, [], name: @name)
 
-  def init(_) do
+  def init(state) do
     IO.puts("Creating ETS #{@name}")
     :ets.new(:currency_api_cache, [:set, :named_table])
-    {:ok, "ETS created"}
+    {:ok, state}
   end
 
   def insert(key, date, rate) do
     GenServer.call(@name, {:insert, {key, date, rate}})
   end
 
+  def retrieve(key, date) do
+    GenServer.call(@name, {:retrieve, {key, date}})
+  end
+
   def handle_call({:insert, data}, _ref, state) do
     :ets.insert_new(:currency_api_cache, data)
     {:reply, :ok, state}
+  end
+
+  def handle_call({:retrieve, {key, date}}, _ref, state) do
+    cached_data = :ets.match_object(:currency_api_cache, {key, date, :"$3"})
+    case Enum.empty?(cached_data) do
+      true -> {:reply, state, []}
+      false ->
+        [{_, _, rate}] = cached_data
+        {:reply, rate, []}
+    end
   end
 end
--- lib/currency_conversion_web/controllers/page_controller.ex
+++ lib/currency_conversion_web/controllers/page_controller.ex
@@ -1,6 +1,7 @@
 defmodule CurrencyConversionWeb.PageController do
   use CurrencyConversionWeb, :controller
   alias CurrencyConversion.Converter
+  alias CurrencyConversion.ApiCache
 
   def index(conn, _params) do
     amount_from_session = get_session(conn, :converted_amount)
@@ -13,11 +14,16 @@ defmodule CurrencyConversionWeb.PageController do
     |> render("index.html", %{converted_amount: converted_amount, error: is_error})
   end
 
-  def convert(conn, %{"amount" => amount, "from" => from, "to" => to, "date" => date}) do
-    conversion_result = Converter.get_historical_rate(from, to, date, amount)
+  def convert(conn, %{"amount" => input_amount, "from" => from, "to" => to, "date" => date}) do
+    {amount_as_float, ""} = Float.parse(input_amount)
+    amount = amount_as_float * 100
+    result = case rate_cached?(from<>to, date) do
+      {:ok, %{rate: rate}} -> calculate_result(rate, amount)
+      {:empty} -> handle_api_call(from, to, date, amount/100)
+    end
 
     conn
-    |> update_session(conversion_result)
+    |> update_session(result)
     |> redirect(to: Routes.page_path(conn, :index))
   end
 
@@ -29,5 +35,30 @@ defmodule CurrencyConversionWeb.PageController do
   defp update_session(conn, {:error, message: _}) do
     conn
     |> put_session(:error, true)
+  end
+
+  defp rate_cached?(key, date) do
+    rate = ApiCache.retrieve(key, date)
+    if rate do
+      {:ok, %{rate: 100000000000}}
+    else
+      {:empty}
+    end
+  end
+
+  defp handle_api_call(from, to, date, amount) do
+    result = Converter.get_historical_rate(from, to, date, amount)
+    placeholder_rate = 5
+    case result do
+      {:ok, converted_amount: converted_amount} ->
+        ApiCache.insert(from<>to, date, placeholder_rate)
+        {:ok, converted_amount: converted_amount}
+      {:error, _} -> {:error, true}
+    end
+  end
+
+  defp calculate_result(rate, amount) do
+    converted_amount = (rate * amount)/100
+    {:ok, converted_amount: converted_amount}
   end
 end
